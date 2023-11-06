0959d19a4e148c0b6d57ccae5d6a0a557151d75c
IsaureStiffel <zoziore.sti@gmail.com>
2023-11-06 20:19:08 +0100

    Progress

---
+++ README.md
@@ -0,0 +1,17 @@
+# Progress
+
+## The dataset
+
+I used a dataset on summary of books to classify them by genre :
+
+https://www.kaggle.com/code/alperkaraca1/book-genre-prediction
+
+## Where I am
+
+I explored the dataset. 
+I preprocessed the data first by a python file with simple preprocessing, then I explored the datas to find more way to simplify my datas. 
+In the final processing file I added the suppression of most common words and of words appearing only one time. 
+
+I then created simple models with BoW and TF-IDF methods and some model of machine learning. 
+
+I am now trying to get better performance of my model. 
---
+++ preprocessing.py
@@ -0,0 +1,18 @@
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+def preprocess_text(text):
+    lemmatizer = WordNetLemmatizer()
+    stop_words = set(stopwords.words('english'))
+
+    # Tokenization
+    tokens = word_tokenize(text)
+    # Remove punctuation and convert to lowercase
+    words = [word.lower() for word in tokens if word.isalnum()]
+    # Remove stopwords
+    words = [word for word in words if word not in stop_words]
+    # Lemmatization using WordNetLemmatizer
+    words = [lemmatizer.lemmatize(word) for word in words]
+    
+    return ' '.join(words)
